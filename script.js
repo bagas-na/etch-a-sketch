1ab7df124f98e0de3ca06893651f38d689816077
bagas-na <bagasnurseno@gmail.com>
2023-01-16 08:09:57 +0700

    Add variable size grid, still cannot dynamically change

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
-let gridLength = 16;
+const lengthSelector = document.querySelector('.grid-size');
+const gridContainer = document.querySelector('.grid');
+const gridLength = document.querySelector(':root');
 
-for(let i=1; i<=16; i++){
-    for(let j=1; 1<=16; j++){
-        
-    }
+console.log(gridContainer.style.gridTemplateColumns);
+
+let gridSize = 16;
+gridLength.style.setProperty('--grid-length', gridSize);
+
+console.log(lengthSelector.value);
+
+createGrid(gridSize);
+
+function createGrid(gridSize) {
+    
+    const box = new Array(gridSize);
+    for(let i=1; i<=gridSize; i++){
+        box[i] = new Array(gridSize);
+        for(let j=1; j<=gridSize; j++){
+            box[i][j] = document.createElement("div");
+            box[i][j].classList.add('box');
+            box[i][j].setAttribute('id', `box-#${j+(i-1)*gridSize}`)
+            gridContainer.appendChild(box[i][j]);
+        }
+    }    
 }
+
+function deleteGrid() {
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+      }
+}
+
+
+lengthSelector.addEventListener('input', function (e) {
+    console.log(e.target.value);
+    gridSize = e.target.value;
+    deleteGrid();
+    createGrid(gridSize);
+});
+
